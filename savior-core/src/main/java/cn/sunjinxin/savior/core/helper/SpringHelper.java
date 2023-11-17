589809e041e7d8e0a28051efa7d297462a8a9746
savior <issavior@163.com>
2023-11-18 02:07:11 +0800

    +SpringHelper

---
+++ savior-core/src/main/java/cn/sunjinxin/savior/core/helper/SpringHelper.java
@@ -0,0 +1,43 @@
+package cn.sunjinxin.savior.core.helper;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.PriorityOrdered;
+
+import javax.annotation.Nonnull;
+
+/**
+ * spring helper
+ * <p>BeanFactoryPostProcessor & PriorityOrdered can priority loading</p>
+ *
+ * @author issavior
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SpringHelper implements BeanFactoryPostProcessor, ApplicationContextAware, PriorityOrdered {
+
+    static ApplicationContext applicationContext;
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    @Override
+    public void postProcessBeanFactory(@Nonnull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setApplicationContext(@Nonnull ApplicationContext applicationContext) throws BeansException {
+        SpringHelper.applicationContext = applicationContext;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
